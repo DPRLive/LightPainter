b71417f34177609e57cfe5f425cd13fb2884c56c
Imusun <imusun190812@gmail.com>
2022-07-21 17:24:52 +0900

    init

---
+++ Source/LightPainter/HandController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HandController.h"
+
+AHandController::AHandController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("MotionController"));
+	SetRootComponent(MotionController);
+	MotionController->SetTrackingSource(EControllerHand::Right); // 오른손에 설정
+	MotionController->SetShowDeviceModel(true); // 자동으로 모델 인식
+}
+
+void AHandController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AHandController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
